439bd8f9a27f7f51d78f37e263a66c591d16209f
Pradnyesh Gore <gorillapie200@gmail.com>
2021-05-03 15:12:21 -0700

    Add enableIf case

--- Components/API/EnableIfExp.h
+++ Components/API/EnableIfExp.h
@@ -15,6 +15,16 @@ namespace
         return inArg + 1;
     }
 
+    template <
+        typename T,
+        typename Z,
+        std::enable_if_t<std::is_same<T, Z>::value>* = nullptr>
+    void TypeEqualitycheck(T inFirst, Z inSecond)
+    {
+        std::cout << "Types are the same " << std::endl;
+    }
+
+
 }   // unnamed namespace
 
 namespace Components
@@ -25,6 +35,16 @@ namespace Components
             FloatingPointEnforcer<float>(44.0f);
             FloatingPointEnforcer<double>(44.0);
             // FloatingPointEnforcer<int>(44); // should fail to compile
+        } // unnamed
+        
+        {
+            std::uint32_t value_uint32_0 = 3;
+            std::uint32_t value_uint32_1 = 4;
+            std::int64_t value_int64_0 = 4;
+            
+            // TypeEqualitycheck(value_uint32_0, value_int64_0);    // should  fail since uint32 != int64
+            TypeEqualitycheck(value_uint32_0, value_uint32_1);
+            TypeEqualitycheck(value_uint32_0, static_cast<std::uint32_t>(value_int64_0));
         }
     }
 } // Components
